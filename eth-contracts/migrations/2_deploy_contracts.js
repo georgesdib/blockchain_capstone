6fe5792a2652962543e3cd485f6e37530efd2987
Georges Dib <georges.dib@gmail.com>
2021-07-10 20:59:07 +0100

    Bring back the non open zeppelin implementation

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-let SolnSquareVerifier = artifacts.require("./SolnSquareVerifier");
+let SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 let CapstoneERC721Token = artifacts.require("CapstoneERC721Token");
 let Verifier = artifacts.require("Verifier");
 
