e868c7d0dca99fe726d8b89712335ac73767d0be
Georges Dib <georges.dib@gmail.com>
2021-07-10 09:50:47 +0100

    Adding SquareVerifier
    - Move and renamed the sokrates generated solidity file
    - Deployed the contract
    - Added testing for verifyTx

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 // migrating the appropriate contracts
-//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 //var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 let CapstoneERC721Token = artifacts.require("CapstoneERC721Token");
+let Verifier = artifacts.require("Verifier");
 
 module.exports = function(deployer) {
   deployer.deploy(CapstoneERC721Token);
-//  deployer.deploy(SquareVerifier);
+  deployer.deploy(Verifier);
 //  deployer.deploy(SolnSquareVerifier);
 };
